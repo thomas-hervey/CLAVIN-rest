d3377de4f09b18e1d9434ed52653cea71b46071f
Travis L Pinney <tpinney@bericotechnologies.com>
2014-02-21 09:44:55 -0500

    Add basic Leaflet integration

--- src/main/resources/assets/js/clavin.js
+++ src/main/resources/assets/js/clavin.js
@@ -2,7 +2,7 @@
 
 function submit() {
     //alert($("#id_textarea").val());
-    console.log($("#id_textarea").val());
+    //console.log($("#id_textarea").val());
     $.ajax({
 	url: "api/v0/geotag",
 	type: "POST",
@@ -12,8 +12,23 @@ function submit() {
 	.done(function(data) {
 	    //console.log(data);
 	    $("#id_results").text(JSON.stringify(data));
+
+	    // add results to map
+	    var bounds = [];
+	    $.each(data.resolvedLocations, (function (idx, val) {
+		//console.log(val);
+		bounds.push([val.geoname.latitude, val.geoname.longitude]);
+		L.marker([val.geoname.latitude, val.geoname.longitude]).addTo(map)
+		    .bindPopup(val.geoname.name);
+	    }));
+	    
+	    // zoom to bounds of markers
+	    map.fitBounds(bounds);
+		
 	});
 }
+
+
 
 
 
